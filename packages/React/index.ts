bc718ea91b0c7d652a78c7649830977a49de874a
Jamie Bower <j_bower@hotmail.co.uk>
2023-07-30 15:42:26 +1000

    chore: remove comments and fix folder structure

--- packages/React/index.ts
+++ packages/React/index.ts
@@ -1,4 +1,4 @@
-import { reRender } from "./ReactDOM";
+import { reRender } from "../ReactDOM";
 
 const React: { createElement: any; initialRender: any[] } = {
     createElement(tag, props, ...children) {
@@ -57,51 +57,3 @@ export function useState(initialState) {
 }
 
 export default React;
-
-// const React = {
-//     createElement: (tag, props, ...children) => {
-//         if (typeof tag === "function") {
-//             return tag(props);
-//         }
-
-//         // Filter out source and self from props
-//         const { __source, __self, ...restProps } = props;
-
-//         const element = {
-//             tag,
-//             props: {
-//                 ...restProps,
-//                 children,
-//             },
-//         };
-
-//         return element;
-//     },
-//     state: {
-//         counter: 0,
-//         value: [],
-//     },
-//     useState(initialState) {
-//         // Take a snapshot of the current counter so that we can increment the counter before we read the current state
-//         const currentCounter = React.state.counter;
-
-//         // Increment the counter to be used by the next instance of useState
-//         React.state.counter++;
-
-//         // If the state doesn't exist yet, initialize it
-//         if (React.state.value[currentCounter] === undefined) {
-//             React.state.value[currentCounter] = initialState;
-//         }
-
-//         function setState(newState) {
-//             // Update the state
-//             React.state.value[currentCounter] = newState;
-
-//             // Reset the counter then rerender everything
-//             React.state.counter = 0;
-//             ReactDOM.reRender();
-//         }
-
-//         return [React.state.value[currentCounter], setState];
-//     },
-// };
