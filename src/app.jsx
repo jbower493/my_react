79d3dfeedc7b7d555a7ea9d1d4706ea0f1650d20
Jamie Bower <j_bower@hotmail.co.uk>
2024-06-15 13:18:33 +1000

    feat: start to test useEffect more, it doesn't work when trying to set state after a fetch

--- src/app.jsx
+++ src/app.jsx
@@ -28,41 +28,81 @@ import React from "../packages/react";
 //     );
 // }
 
-function AppKid() {
-    const [secondCount, setSecondCount] = React.useState(0);
+// function AppKid() {
+//     const [secondCount, setSecondCount] = React.useState(0);
+
+//     React.useEffect(() => {
+//         console.log("my first effect ran");
+//     }, []);
+
+//     React.useEffect(() => {
+//         console.log("my second effect ran");
+//     }, [secondCount]);
+
+//     React.useEffect(() => {
+//         console.log("my third effect ran");
+//     });
+
+//     return (
+//         <div>
+//             <p>{secondCount}</p>
+//             <button onClick={() => setSecondCount(secondCount + 1)}>
+//                 Increment second
+//             </button>
+//             <h4>Count</h4>
+//         </div>
+//     );
+// }
+
+// export default function App() {
+//     const [count, setCount] = React.useState(0);
+
+//     return (
+//         <div>
+//             <p>{count}</p>
+//             <button onClick={() => setCount(count + 1)}>Increment</button>
+//             <h2>Count</h2>
+//             <AppKid />
+//         </div>
+//     );
+// }
+
+function Users({ name }) {
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [users, setUsers] = React.useState(null);
 
     React.useEffect(() => {
-        console.log("my first effect ran");
+        setIsLoading(true);
+
+        fetch("https://randomuser.me/api/")
+            .then((res) => res.json())
+            .then((data) => {
+                setIsLoading(false);
+                // setUsers(data);
+            });
     }, []);
-
-    React.useEffect(() => {
-        console.log("my second effect ran");
-    }, [secondCount]);
-
-    React.useEffect(() => {
-        console.log("my third effect ran");
-    });
 
     return (
         <div>
-            <p>{secondCount}</p>
-            <button onClick={() => setSecondCount(secondCount + 1)}>
-                Increment second
-            </button>
-            <h4>Count</h4>
+            <h2>{name || "Default Users Page Name"}</h2>
+            {isLoading ? <div>Loading users...</div> : <div>Users fetched</div>}
+            {/* <div>{users?.info?.seed || "Nothing yet"}</div> */}
         </div>
     );
 }
 
 export default function App() {
-    const [count, setCount] = React.useState(0);
+    const [usersPageName, setUsersPageName] = React.useState("");
 
     return (
         <div>
-            <p>{count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-            <h2>Count</h2>
-            <AppKid />
+            <h1>App</h1>
+            <input
+                placeholder="Set users page name"
+                value={usersPageName}
+                onChange={(e) => setUsersPageName(e.target.value)}
+            />
+            <Users name={usersPageName} />
         </div>
     );
 }
